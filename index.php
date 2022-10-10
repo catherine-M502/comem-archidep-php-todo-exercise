79989ff798b2336681e91399215e1d6ed75e24e8
Alexandre Saul <alexandre.saul@heig-vd.ch>
2022-10-10 09:07:40 +0200

    modif sql all task

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo'; // IMPLEMENT ME (IMPLEMENT DONE)
 $items = $db->query($selectQuery);
 ?>
 
